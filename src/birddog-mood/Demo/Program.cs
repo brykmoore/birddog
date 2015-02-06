7229d392024e0e8c3cb9a90b78f9df10db396361
bmoore <brykmoore@gmail.com>
2015-02-05 22:34:31 -0500

    updates

--- src/birddog-mood/Demo/Program.cs
+++ src/birddog-mood/Demo/Program.cs
@@ -11,11 +11,60 @@ using MoodFinder.StringMatching;
 
 namespace Demo
 {
+    public static class EntityFrameworkUtil
+    {
+        public static IEnumerable<T> QueryInChunksOf<T>(this IQueryable<T> queryable, int chunkSize)
+        {
+            return queryable.QueryChunksOfSize(chunkSize).SelectMany(chunk => chunk);
+        }
+
+        public static IEnumerable<T[]> QueryChunksOfSize<T>(this IQueryable<T> queryable, int chunkSize)
+        {
+            int chunkNumber = 0;
+            while (true)
+            {
+                var query = (chunkNumber == 0)
+                    ? queryable
+                    : queryable.Skip(chunkNumber * chunkSize);
+                var chunk = query.Take(chunkSize).ToArray();
+                if (chunk.Length == 0)
+                    yield break;
+                yield return chunk;
+                chunkNumber++;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var text = "cheerful hello fond of";
+            using (var db = new birddogEntities())
+            {
+                db.tweet
+                var count = 0;
+                foreach (var item in db.OrderBy(x=>x.id_str).QueryInChunksOf(1000)) 
+                {
+
+                    //var returnValue = process(item.text);
+                    var positive = 1;// returnValue.Where(x => x.Category == "Positive");
+                    var negative = 1;// returnValue.Where(x => x.Category == "Negative");
+                    if (positive != null)
+                    {
+                        item.bing_positive = 1;//(int)positive.First().Count;
+                    }
+                    if (negative != null) 
+                    {
+                        item.bing_negative = 1;//(int)negative.First().Count;
+                    }
+                    db.SaveChanges();
+                }
+
+            }
+        }
+
+        public static List<MoodFrequencyItem> process(string text){
+
             //lowercase
             text = text.ToLower();
             //strip punctuation
@@ -24,7 +73,7 @@ namespace Demo
 
             var stemmer = new Stemming();
             var trie = new Trie<MoodValue>();
-            var moodList = new MoodListing().MoodList;
+            var moodList = new MoodListing().BingLiuMoodList;
 
             foreach (var item in moodList)
             {
@@ -43,22 +92,24 @@ namespace Demo
             var list = trie.Find(inputString).GroupBy(x => new { x.Category }).Select(g => new MoodFrequencyItem()
             {
                 Category = g.Key.Category,
-                Count = g.Sum(x=>x.Weight)
+                Count = g.Sum(x => x.Weight)
             }).ToList();
 
-            var moodResults = moodList.Select(x => x.Value.Category).Distinct().Select(y => new MoodFrequencyItem {
-            Category = y
+            var moodResults = moodList.Select(x => x.Value.Category).Distinct().Select(y => new MoodFrequencyItem
+            {
+                Category = y
             }).ToList();
             var sum = list.Sum(mood => mood.Count);
 
             foreach (var mood in list)
             {
                 var firstOrDefault = moodResults.FirstOrDefault(x => x.Category == mood.Category);
-                if(firstOrDefault != null)
+                if (firstOrDefault != null)
                 {
                     firstOrDefault.Count = mood.Count / sum;
                 }
             }
+            return list;
         }
     }
 }
